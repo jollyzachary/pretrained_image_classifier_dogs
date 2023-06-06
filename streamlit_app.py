0cccdf206390e6cb1704b1986233472d2cd2a2b1
Zachary Jolly <129251882+jollyzachary@users.noreply.github.com>
2023-06-06 13:21:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-from classify import predict_breed
+from classifier import classifier
 
 # Set the title of the app
 st.title('Dog Breed Classifier')
@@ -14,9 +14,13 @@ if uploaded_file is not None:
     # Display the uploaded image
     st.image(img, caption='Uploaded Image.', use_column_width=True)
 
+    # Save the uploaded image to a temporary file and get the path of the file
+    img_path = 'temp.jpg'
+    img.save(img_path)
+
     # Predict the breed of the dog in the image
-    breed = predict_breed(img)
+    # You can choose the model you want to use here
+    breed = classifier(img_path, 'resnet')
 
     # Display the predicted breed
     st.write("Predicted breed: ", breed)
-
